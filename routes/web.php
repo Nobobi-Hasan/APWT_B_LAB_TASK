cbf5271a13186dd86512e0641e5a167249557d71
Nobobi-Hasan <18-38712-3@student.aiub.edu>
2021-06-23 18:18:49 +0600

    product add

--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,16 @@ Route::get('/system', 'SystemController@index')->name('system.index');
 
 Route::get('/registration', 'RegistrationController@index')->name('registration.index');
 Route::post('/registration', 'RegistrationController@verify')->name('registration.verify');
+Route::get('/system/sales', 'SalesController@index')->name('Sales.index');
+
+Route::get('/system/sales/physical_store', 'PhysicalController@index')->name('Physical.index');
+Route::get('/system/sales/social_media', 'SocialController@index')->name('Social.index');
+Route::get('/system/sales/ecommerce', 'EcommerceController@index')->name('Ecommerce.index');
+
+
+Route::get('/system/product_management', 'ProductController@index')->name('Product.index');
+
+Route::get('/system/product_management/existing_products', 'ProductController@existingIndex')->name('Product.existingIndex');
+Route::get('/system/product_management/upcoming_products', 'ProductController@upcomingIndex')->name('Product.upcomingIndex');
+Route::get('/system/product_management/add_product', 'ProductController@addIndex')->name('Product.addIndex');
+Route::post('/system/product_management/add_product', 'ProductController@addProduct')->name('Product.addProduct');
